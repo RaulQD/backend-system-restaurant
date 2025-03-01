aab9c6ff628cca1eccbf78d1786674c04c015de8
Raul Kevin Quispe Diaz <raul.g.quispe@gmail.com>
2025-03-01 12:25:53 -0500

    add change to mysql.js

--- src/config/mysql.js
+++ src/config/mysql.js
@@ -2,11 +2,11 @@ import mysql from 'mysql2/promise';
 
 
 export const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST || 'localhost',
-  user: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || '1234',
+  host: process.env.MYSQLHOST || 'localhost',
+  user: process.env.MYSQLUSER || 'root',
+  password: process.env.MYSQLPASSWORD || '1234',
   database: process.env.MYSQL_DATABASE || 'sistema_restaurant',
-  port: process.env.MYSQL_PORT || 3306,
+  port: process.env.MYSQLPORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
