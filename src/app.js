a18573e84987e98a8f3438a22c0a9302ca9eb193
Raul Kevin Quispe Diaz <raul.g.quispe@gmail.com>
2025-03-01 11:08:07 -0500

    ADD PORT IN APP.JS

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 
 import server from './server.js';
 
-const PORT = process.env.MYSQL_PORT || 3000;
+const PORT = process.env.PORT
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
